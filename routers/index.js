9b13c3b7865fd0a4797f03764b84ab6397a2267e
viktorir <v1ktorir@yandex.ru>
2023-06-12 15:52:46 +0800

    Added routing to product controller

--- routers/index.js
+++ routers/index.js
@@ -3,10 +3,12 @@ const router = new Router();
 
 const authRouter = require('./auth.router');
 const clientRouter = require('./client.router');
+const productRouter = require('./product.router');
 
 router.get('/', (req, res) => { res.status(200).json({ message: "Hello мир, манера крутит мир" }) });
 
 router.use('/auth', authRouter);
 router.use('/client', clientRouter);
+router.use('/product', productRouter);
 
 module.exports = router;
---
+++ routers/product.router.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+const router = new Router();
+
+const productController = require('../controllers/product.controller')
+
+router.post('/', productController.create);
+router.get('/', productController.getAll);
+router.get('/:id', productController.getOne);
+router.put('/:id', productController.update);
+router.delete('/:id', productController.delete)
+
+module.exports = router;
