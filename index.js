fdbbe88fe190ef14d95bef1384ab2152f0cbce8f
viktorir <v1ktorir@yandex.ru>
2023-06-13 14:27:21 +0800

    Added error handling

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 require('dotenv').config();
 const express = require(`express`);
-const router = require('./routers/index')
+const router = require(`./routers/index`);
+const errorHandler = require(`./middleware/error.handling.middleware`);
 
 const PORT = process.env.PORT || 8000;
 
 const app = express();
 
 app.use(express.json());
-app.use('/api', router);
+app.use(`/api`, router);
+
+app.use(errorHandler)
 
 const start = () => {
     try {
