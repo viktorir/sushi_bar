4233c205cf21a67b50d044ffe90265d589754f22
viktorir <v1ktorir@yandex.ru>
2023-06-15 02:07:22 +0800

    Added package cors (no conf)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require(`dotenv`).config();
 const express = require(`express`);
+const cors = require(`cors`)
 const router = require(`./routers/index`);
 const errorHandler = require(`./middleware/error.handling.middleware`);
 
@@ -7,7 +8,7 @@ const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json(), cors());
 app.use(`/api`, router);
 
 app.use(errorHandler)
